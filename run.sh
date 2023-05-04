e0acfff46b8613d32b9eda8120b8bb13245ce815
rin8351 <format037@gmail.com>
2023-05-04 14:57:59 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 # Run as daemon
-docker run -d -it --network host \
-    -v ./data:/app/data \
-    --name to_chat --restart always to_chat
+docker run -d -it --network host -v ./data:/app/data --name to_chat --restart always to_chat
